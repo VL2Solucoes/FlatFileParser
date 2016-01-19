e152e5742563b143f8024de8c793b39679211568
luizearaujo <luiz.earaujo@gmail.com>
2016-01-19 11:20:23 -0200

    example code included

---
+++ FlatFileParser/FlatFileParser.Example/Entity/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FlatFileParser.Core.Attributes;
+using FlatFileParser.Core.Enums;
+
+namespace FlatFileParser.Example.Entity
+{
+    [ProcessorType(LineProcessingType.Full)]
+    public class Person
+    {
+        [FullProcessorField(1, 5)]
+        public int Id { get; set; }
+
+        [FullProcessorField(6, 20)]
+        public string Name { get; set; }
+
+        [FullProcessorField(26, 8, "ParseExact", "yyyyMMdd")]
+        public DateTime BirthDate { get; set; }
+
+        [FullProcessorField(34, 10, "Parse", "2")]
+        public decimal Salary { get; set; }
+    }
+}
---
+++ FlatFileParser/FlatFileParser.Example/Processor/PersonProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlatFileParser.Core.Processor;
+using FlatFileParser.Example.Entity;
+
+namespace FlatFileParser.Example.Processor
+{
+    public class PersonProcessor
+    {
+        public List<Person> Persons { get; set; } = new List<Person>();
+
+        public PersonProcessor(List<string> lines)
+        {
+            var processor = new FileProcessor<Person>();
+
+            Persons = processor.ProcessLines(lines).ToList();
+
+        }
+    }
+}
